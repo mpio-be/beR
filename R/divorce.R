6756fc881a2d638eb471b70b982882d72d045cc2
valcu <valcu@orn.mpg.de>
2018-10-23 11:04:23 +0200

    pairing status

--- R/divorce.R
+++ R/divorce.R
@@ -1,42 +1,67 @@
 
-#' divorced pairs
+
+#' Divorced pairs
+#'
+#' @param d data.frame containing maleID, femaleID and season
+#'
+#' @return a data.table similar to d but with two additional columns: male_status and female_status
 #' @export
+#'
 #' @examples
 #' data(divorcees)
-#' d = divorcees
-#' o = pairing_status(d)
-
-
-
-
+#' x = pairing_status(divorcees)
+#' x
+ 
 pairing_status <- function(d) {
   o = data.table(d)
 
-  #IN WORK!
 
-  m = merge(o, o, by = 'maleID', suffixes = c('', '_f'))
-
-  # faithful
-  m[season > season_f & femaleID == femaleID_f, male_status := 're-united']
-  
-  #divorces and widows
-  x = m[season > season_f & femaleID != femaleID_f]
-  # is femaleID_f (former) still alive in season?
-  x = merge(x, o[, .(femaleID, season)], by.x = 'femaleID_f', by.y = 'femaleID', 
-      suffixes = c('', '_y'))
-  
-  x[season > season_y, male_status := 'divorced_male']
-  x[season != season_y, male_status := 'widowed_male']
+  # FAITHFUL pairs
+    rpairs = merge(o, o, by = c('maleID', 'femaleID'), suffixes = c('', '_f'), allow.cartesian = TRUE)
+    rpairs = rpairs[season > season_f  , .(maleID, femaleID, season) ] %>% unique
+    rpairs[, ':='  (male_status = 're-united',  female_status = 're-united') ]
 
 
-  rbind(m[!is.na(male_status), .(maleID,season, male_status)], 
-        x[!is.na(male_status), .(maleID,season, male_status)])
+  #MALES divorces and widows
+    m = merge(o, o, by = 'maleID', suffixes = c('', '_f'), allow.cartesian = TRUE)
+    x = m[season > season_f & femaleID != femaleID_f]
+
+    # is femaleID_f (former) still alive in season?
+    z = merge( x[, .(maleID, femaleID_f, season)], o[, .(femaleID, season)], by.x = c('femaleID_f', 'season'), by.y = c('femaleID', 'season') )
+    z[, male_status := 'divorced']
+    dw = merge(x, z[, .(maleID, season, male_status)] , by = c('maleID', 'season') , all.x = TRUE )
+    dw[is.na(male_status), male_status := 'widowed']
+    dwmales = dw[, .(maleID, season,male_status)]%>% unique
 
 
 
+  #FEMALES divorces and widows
+    m = merge(o, o, by = 'femaleID', suffixes = c('', '_f'), allow.cartesian = TRUE)
+    x = m[season > season_f & maleID != maleID_f]
+
+    # is maleID_f (former) still alive in season?
+    z = merge( x[, .(femaleID, maleID_f, season)], o[, .(maleID, season)], by.x = c('maleID_f', 'season'), by.y = c('maleID', 'season') )
+    z[, female_status := 'divorced']
+    dw = merge(x, z[, .(femaleID, season, female_status)] , by = c('femaleID', 'season') , all.x = TRUE )
+    dw[is.na(female_status), female_status := 'widowed']
+    dwfemales = dw[, .(femaleID, season,female_status)]%>% unique
+
+
+  # all
+    xx = rbindlist( list(rpairs, dwmales, dwfemales), fill = TRUE)
 
 
 
+    O = merge(o, rpairs,  by = c('maleID', 'femaleID', 'season'), all.x = TRUE )
 
-}
+    O = merge(O, dwmales,  by = c('maleID', 'season'), all.x = TRUE , suffixes = c('', '_temp'))
+    O[!is.na(male_status_temp), male_status := male_status_temp][, male_status_temp := NULL]
+
+    O = merge(O, dwfemales,  by = c('femaleID', 'season'), all.x = TRUE , suffixes = c('', '_temp'))
+    O[!is.na(female_status_temp), female_status := female_status_temp][, female_status_temp := NULL]
+
+
+    O[, .(maleID, femaleID, season, male_status, female_status)]
+
+  }
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # beR
 Behavioural Ecology tools in R
+
+### function list
+* `pairing_status()` assigns change in pairing status (divorcees, widowed or re-united)
+* `bCounter()`       counts independent bouts. `bCounter(c('a', 'a', 'a', 2, 3,3, NA, 5))` returns `1 1 1 2 3 3 4 5`
+
